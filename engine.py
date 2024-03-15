9df594002a37f3c11e7cb30cf2aabc338e79424a
Yuriy Petrov <Jurijp2005@gmail.com>
2024-03-15 08:52:21 +0100

    commit2

--- engine.py
+++ engine.py
@@ -1,17 +1,15 @@
 #Will contain input and information about current chess game. Determine valid moves (extra. keep a move log)
 
-class CurrentGame():
+class GameState():
     def _init_(self):
-        #Each element contain 2 characters. Board is a 2dlist
         self.board = [
-            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR",],
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR",],   
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP",],
             ["--", "--", "--", "--", "--", "--", "--", "--",],
             ["--", "--", "--", "--", "--", "--", "--", "--",],
             ["--", "--", "--", "--", "--", "--", "--", "--",],
             ["--", "--", "--", "--", "--", "--", "--", "--",],
             ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP",],
-            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR",]
-        ]
-        self.moveLog = []
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR",]]
         self.whiteTurnMove = True
+        self.moveLog = []
--- main.py
+++ main.py
@@ -11,43 +11,67 @@ import engine
 pygame.init()
 
 #Some initial variables
-width = 512
-height = 512
+width = height = 512 #squares 64px64p
 dimension = 8 #Board size 8x8
-squareSize = height & width // dimension #Math/ 8x8 = 64 512/8 = 64. squareSize 64x64 pixels.
+squareSize =  height// dimension #Math/ 8x8 = 64 512/8 = 64. squareSize 64x64 pixels.
 images = {} #will use as a dictionary of images. images are taken from here https://drive.google.com/drive/folders/1qH7IQj5lj7o3MQIb5TAZhsDr_5f9p8aq
 
-#Size for game screen and name
-screen = pygame.display.set_mode([width, height])
-pygame.display.set_caption("Chess")
 
-#GameRunning /press x to quit
-gameOn = True
-while gameOn:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameOn = False
 
-#GameTime Used to control frameRate of the game
-timer = pygame.time.Clock()
-fps = 60
+
+
 
 
             
-#-------------------------------------------------
-'''
-I use this way instead of using 
-images["wP] = pygame.image.load("images/wp.png) to save time and space. Found this here:
-https://stackoverflow.com/questions/65431717/efficient-way-to-load-mass-amounts-of-images-in-pygame
-'''
+            
+#----------------------------------------------------------------------------------------------------
+#I use this way instead of using 
+#images["wP] = pygame.image.load("images/wp.png) to save time and space. Found this here:
+#https://stackoverflow.com/questions/65431717/efficient-way-to-load-mass-amounts-of-images-in-pygame
+
 #Chess piece images
 def loadImages():
     pieces = ["wP", "wR", "wN", "wB", "wK", "wQ", "wB", "wN", "wR", "bP", "bR", "bN", "bB", "bK", "bQ", "bB", "bN", "bR"]
-    for piece in pieces:
-        {
+    for piece in pieces:    
+        images[piece] = pygame.transform.scale(pygame.image.load("images/" + piece + ".png")), (squareSize, squareSize)  #scale images to square size
+        #write "images[wP]" to acess picture from the library.
+    
+#----------------------------------------------------------------------------------------------------
+#handle user input and graphics
+def main():
+    
+    #Size for game screen and name
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Chess")
+    timer = pygame.time.Clock()
+    fps = 60
+    screen.fill(pygame.Color("white"))
+    gs = engine.GameState()  #gs = gamestate
+    drawGameState(screen, gs)
+    loadImages()
+    
+    #GameRunning /press x to quit
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            timer.tick(fps)
+            pygame.display.flip()
             
-        }
-    images[piece] = pygame.transform.scale(pygame.image.load("images/" + piece + ".png")), (squareSize, squareSize)  #scale images to square sizes.
-    #write "images[wP]" to acess picture from the library.
+    
+#Graphics and drawing.
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+ 
+#draw squares   
+def drawBoard(screen):
+ 
 
-
+#draw pieces  
+def drawPieces(screen, gs):
+    
+    
+if __main__ == "__main__":
+    main()
